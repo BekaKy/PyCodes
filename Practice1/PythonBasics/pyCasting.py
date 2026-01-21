eaf72f13a8896d8cf2a4b87b9fb628a06a1d0d4e
BekaKy <bekaryskuanyshev0906@gmail.com>
2026-01-21 09:44:33 +0500

    Added Python Basiscs example codes from W3Schools

---
+++ Practice1/PythonBasics/pyCasting.py
@@ -0,0 +1,8 @@
+# There may be times when you want to specify a type on to a variable. 
+# This can be done with casting. 
+# Python is an object-orientated language, and as such it uses classes to define data types, including its primitive types.
+
+# Casting in python is therefore done using constructor functions:
+x = int(1)   # int() - constructs an integer number from an integer literal, a float literal (by removing all decimals), or a string literal (providing the string represents a whole number)
+y = float(2.8)   # float() - constructs a float number from an integer literal, a float literal or a string literal (providing the string represents a float or an integer)
+z = str(3.0)  # str() - constructs a string from a wide variety of data types, including strings, integer literals and float literals.

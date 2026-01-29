fdac92d7b26e5728c86d3ef275ec1c501be7fc97
BekaKy <bekaryskuanyshev0906@gmail.com>
2026-01-29 09:13:00 +0500

    Added lists examples

---
+++ Practice2/PythonLists/lists.py
@@ -0,0 +1,7 @@
+# Lists are used to store multiple items in a single variable.
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+# Since lists are indexed, lists can have items with the same value:
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+# To determine how many items a list has, use the len() function:

f94fbbf31363d4ff124fe5c7d0cf79c5e685b43e
BekaKy <bekaryskuanyshev0906@gmail.com>
2026-01-29 09:12:20 +0500

    Added tuple examples

---
+++ Practice2/PythonLists/tuple.py
@@ -0,0 +1,15 @@
+# Tuples are used to store multiple items in a single variable.
+# A tuple is a collection which is ordered and unchangeable.
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+# Ordered
+
+# When we say that tuples are ordered, it means that the items have a defined order, and that order will not change.
+# Unchangeable
+
+# Tuples are unchangeable, meaning that we cannot change, add or remove items after the tuple has been created.
+# Allow Duplicates
+
+# Since tuples are indexed, they can have items with the same value
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)

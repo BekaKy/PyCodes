9dc726d472e25ad752c736cf9cc74dc7c08b8c9a
BekaKy <bekaryskuanyshev0906@gmail.com>
2026-01-29 08:58:37 +0500

    Added more examples to Elif statements

--- Practice2/If_else_statements/pyElif.py
+++ Practice2/If_else_statements/pyElif.py
@@ -9,3 +9,13 @@ elif a == b:
   print("a and b are equal")
 
 # You can have as many elif statements as you need. Python will check each condition in order and execute the first one that is true.
+score = 75
+
+if score >= 90:
+  print("Grade: A")
+elif score >= 80:
+  print("Grade: B")
+elif score >= 70:
+  print("Grade: C")
+elif score >= 60:
+  print("Grade: D")

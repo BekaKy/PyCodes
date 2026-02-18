395c76bf1dff3ffdeb906b5ce3e8cd06cf1641e7
BekaKy <bekaryskuanyshev0906@gmail.com>
2026-02-18 21:18:14 +0500

    Added practice tasks on classes

---
+++ Practice3/classes/class_variables.py
@@ -0,0 +1,43 @@
+# Properties are variables that belong to a class. They store data for each object created from the class.
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Emil", 36)
+
+print(p1.name)
+print(p1.age) 
+# You can access object properties using dot notation:
+class Car:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+car1 = Car("Toyota", "Corolla")
+
+print(car1.brand)
+print(car1.model) 
+# You can modify the value of properties on objects:
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Tobias", 25)
+print(p1.age)
+
+p1.age = 26
+print(p1.age) 
+# You can delete properties from objects using the del keyword:
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Linus", 30)
+
+del p1.age
+
+print(p1.name) # This works
+# print(p1.age) # This would cause an error 
